50c80efda1c2ce35a36a45bb9f0b11714e7c33ba
albiecr <albertinacrodrigues@gmail.com>
2025-06-25 06:43:26 -0300

    feat(metodo): Lista<Motorista> em MotoristaDAO

--- TaxiSistema/src/com/DAO/MotoristaDAO.java
+++ TaxiSistema/src/com/DAO/MotoristaDAO.java
@@ -2,7 +2,10 @@ package com.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.conexao.ConnectionFactory;
 import com.heranca.Motorista;
@@ -19,5 +22,23 @@ public class MotoristaDAO {
 			stmt.setString(4, motorista.getCnh());
 			stmt.executeUpdate();
 		}
+	}
+	
+	public List<Motorista> selecionarTodos() throws SQLException{
+		List<Motorista> motoristas = new ArrayList<Motorista>();
+		String sql = "SELECT * FROM motorista";
+		try (Connection conn = ConnectionFactory.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(sql);
+				ResultSet rs = stmt.executeQuery()) {
+					while(rs.next()) {
+						Motorista motorista = new Motorista (
+								rs.getNString("nome"),
+								rs.getString("endereco"),
+								rs.getString("telefone"),
+								rs.getString("cnh"));
+						motoristas.add(motorista);
+					}
+				}
+			return motoristas;
 	}
 }
